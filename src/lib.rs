974788b5c94a283e33c4ec3cfc908f574afea5f2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-07-20 11:21:20 +0530

    Adds unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,27 @@ fn parse_string(dsn: &str) -> PyResult<Pcb> {
     tracing::info!("Parsing content\n\n{dsn}");
     Ok(Pcb::default())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use tracing_test::traced_test;
+
+    #[test]
+    #[traced_test]
+    fn test_parser() {
+        for dsn_file in glob::glob("./tests/*.dsn")
+            .expect("failed to read glob pattern")
+            .flatten()
+        {
+            tracing::info!("Parsing file {dsn_file:?}...");
+            parse_file(
+                dsn_file
+                    .into_os_string()
+                    .into_string()
+                    .expect("failed to convert to String"),
+            )
+            .unwrap();
+        }
+    }
+}
