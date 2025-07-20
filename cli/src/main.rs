0f1f48f53aa8824f85891294e2266e92d3d2dbdd
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-07-20 12:04:03 +0530

    temp

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+//! A small cli utility to print parsed output as JSON.
+
+use dsn_parser;
+use std::path::Path;
+
+fn main() -> anyhow::Result<()> {
+    // First argument is always a file.
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 1 {
+        println!("USAGE: {} <file_name>", env!("CARGO_BIN_NAME"));
+        std::process::exit(0);
+    }
+
+    let infile = Path::new(&args.first().expect("no valid path"));
+    anyhow::ensure!(infile.exists(), "{infile:?} doesn't exists");
+    tracing::info!("Parsing file {infile:?}");
+    let result = dsn_parser::parse_file(infile)?;
+    println!("{result:?}");
+    Ok(())
 }
