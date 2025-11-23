ea5d240b014c3823f70a246137cc0b2d93ffe1d5
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-11-23 12:21:55 +0530

    cli as example to quickly parse a file

---
+++ dsn-parser/examples/cli.rs
@@ -0,0 +1,23 @@
+//! A small cli utility to print parsed output as JSON.
+
+use argh::FromArgs;
+use std::path::Path;
+
+#[derive(FromArgs)]
+/// Parse DSN flie and output JSON
+struct Cli {
+    /// dsn file.
+    #[argh(positional)]
+    infile: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    // First argument is always a file.
+    let cli: Cli = argh::from_env();
+    let infile = Path::new(&cli.infile);
+    anyhow::ensure!(infile.exists(), "{infile:?} doesn't exists");
+    tracing::info!("Parsing file {infile:?}");
+    let result = dsn_parser::parse_file(infile.to_string_lossy().into())?;
+    println!("{result:?}");
+    Ok(())
+}
